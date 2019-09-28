5c30273a789f67e079298280ec774f5b64ebd173
oscar leon <oscar.leon.delmar@gmail.com>
2019-09-28 14:56:19 -0500

    lesson routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Router, Switch, Route, Link} from 'react-router-dom'
+
+import pokemon from './pokemon.json';
+
+import PageOne from './components/PageOne';
+import PageTwo from './components/PageTwo';
+import PageThree from './components/PageThree';
+
+
+import PokemonList from './components/PokemonList';
+import PokemonDetails from './components/PokemonDetails';
+
+
+
+
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      pokemon: pokemon
+    }
+  }
+
+  render(){
+      return (
+        <div >
+          <Link to="/">Home</Link>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/pokemon">Pokemon  List</Link>
+
+          <div>
+            <br /><br /><br /><br /><br />
+
+          </div>
+
+        
+          <Switch>
+
+              <Route exact path='/page' component={PageOne}/>
+              <Route path='/page/two' component={PageTwo}/>
+              <Route path='/three' component={PageThree}/>
+
+
+              <Route exact path='/pokemon' render = {(props) => <PokemonList {...props} list={this.state.pokemon} />} />
+
+              <Route path='/pokemon/:id' render = {(props) => <PokemonDetails {...props} list={this.state.pokemon} />} />
+
+          
+
+
+
+          </Switch>
+        
+
+
+      
+        </div>
+      );
+  }
 }
 
 export default App;
